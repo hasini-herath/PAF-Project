d713d692252ef58158fe9428cf2fe22c66b708da
User <User@DESKTOP-QUQH14T>
2021-04-22 16:21:35 +0530

    login function

---
+++ Buyer/src/main/java/com/DBConnect.java
@@ -0,0 +1,21 @@
+package com;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnect {
+	private Connection connect()
+	 {
+	 Connection con = null;
+	 try
+	 {
+	 Class.forName("com.mysql.jdbc.Driver");
+
+	 
+	 con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/buyer", "root", "Root@123");
+	 }
+	 catch (Exception e)
+	 {e.printStackTrace();}
+	 return con;
+	 } 
+}
